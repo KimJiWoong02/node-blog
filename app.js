f5258c4c1c6e4f3e955bfa4efb564a7864700d04
KimJiWoong02 <jiwoongkim02@gmail.com>
2022-04-13 10:50:03 +0900

    라이브러리 정리

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connect = require('./schemas');
 const ejs = require("ejs");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
@@ -20,13 +21,14 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
+app.use(cors());
 app.use(requestMiddleware);
 
 app.use("/api", [postRouter, commentRouter]);
 
 
 app.get('/', (req, res) => {
-    return res.render("mainpage");
+    return res.render("mainpage"); 
 });
 
 app.listen(port, () => {
